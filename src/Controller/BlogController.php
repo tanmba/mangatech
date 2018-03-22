0ba08d7c8796ada12fd2b8dc95195492a5eb1c60
tanmba <tan-mba.takougnadi@hetic.net>
2018-03-22 16:52:59 +0100

    create profile mangalis and manga vues

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\MangasRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Mangas;
 
 class BlogController extends Controller
 {
@@ -24,6 +26,42 @@ class BlogController extends Controller
     {
         return $this->render('login.html.twig', [
             'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile()
+    {
+        return $this->render('profile.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/mangalist", name="mangalist")
+     */
+    public function mangalist(MangasRepository $mangasRepository)
+    {
+        $mangas = $mangasRepository->findAll();
+
+        return $this->render('mangalist.html.twig', [
+            'controller_name' => 'BlogController',
+            'mangas' => $mangas,
+        ]);
+    }
+
+    /**
+     * @Route("/manga", name="manga")
+     */
+    public function manga(MangasRepository $mangasRepository)
+    {
+        $mangas = $mangasRepository->findAll();
+
+        return $this->render('manga.html.twig', [
+            'controller_name' => 'BlogController',
+            'mangas' => $mangas,
         ]);
     }
 }
