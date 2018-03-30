a9de54270c45dd277bf9d385180f74daa547c2d9
DupreC <dupre.col@gmail.com>
2018-03-30 17:05:31 +0200

    update user+manga

--- src/Controller/MangaController.php
+++ src/Controller/MangaController.php
@@ -39,8 +39,9 @@ class MangaController extends Controller
             'mangas' => $mangas,
         ]);
     }
+
     /**
-     * @Route("/addmanga", name="home")
+     * @Route("/addmanga", name="addmanga")
      */
     public function newMangas(Request $request)
     {
@@ -80,6 +81,32 @@ class MangaController extends Controller
                 'form' => $form->createView()
             ]);
     }
+    /**
+     * @Route("/updatemanga/{id}", name="updatemanga")
+     */
+    public function updateManga(Request $request, MangasRepository $mangasRepository, $id)
+    {
+        $collection = $mangasRepository->find($id);
+        $form = $this->createForm(CollectionType::class, $collection);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($collection);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('mangalist');
+
+        }
+
+        return $this->render('mangas/updatemanga.html.twig',
+            [
+                'form' => $form->createView()
+            ]);
+    }
+
     /**
      * @return string
      */
